f9da8b6f0debc39ed87aa9e5307c7b6d76c711fd
Eshley <sjw2332@gmail.com>
2021-09-02 11:43:14 +0900

    스프링부트 mustache 엔진사용 v1.1

--- src/main/java/com/cos/viewapp/web/FrontController.java
+++ src/main/java/com/cos/viewapp/web/FrontController.java
@@ -31,7 +31,7 @@ public class FrontController {
 		
 		List<Board> boards = new ArrayList<>();
 		boards.add(new Board(1,"제목1"));
-		boards.add(new Board(2,"제목2"));
+		boards.add(new Board(2,"제목3"));
 		
 		model.addAttribute("boards",boards);
 		
